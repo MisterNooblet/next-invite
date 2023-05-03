702c5bcd9f4f0de1219d9354ae889ef0dd691140
misternooblet <dragonroadsupport@gmail.com>
2023-05-03 08:40:55 +0300

    migrated to mySql

--- src/pages/api/utils/connectMySql.ts
+++ src/pages/api/utils/connectMySql.ts
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const environment = process.env.NODE_ENV;
-
+console.log(environment);
 const db = mysql.createConnection({
-  host: environment !== 'development' ? process.env.SQL_HOST : 'localhost',
+  host: process.env.SQL_HOST,
+  port: Number(process.env.SQL_PORT),
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE,
